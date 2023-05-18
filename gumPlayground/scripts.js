98584b5152f2720d47e4e8607eabed934bbe4af9
Robert Bunch <rdbunch@gmail.com>
2023-05-17 22:35:12 -0400

    Beginning of 12. - add showMyFeed - add stopMyFeed

--- gumPlayground/scripts.js
+++ gumPlayground/scripts.js
@@ -1,4 +1,4 @@
-
+const videoEl = document.querySelector('#my-video');
 let stream = null // Init stream var so we can use anywhere
 const constraints = {
     audio: true, //use your headphones, or be prepared for feedback!
@@ -15,8 +15,22 @@ const getMicAndCamera = async(e)=>{
         //user denied access to constraints
         console.log("user denied access to constraints")
     }
-    
-
 };
 
+const showMyFeed = e=>{
+    console.log("showMyFeed is working")
+    videoEl.srcObject = stream; // this will set our MediaStream (stream) to our <video />
+    const tracks = stream.getTracks();
+    console.log(tracks);
+}
+const stopMyFeed = e=>{
+    const tracks = stream.getTracks();
+    tracks.forEach(track=>{
+        // console.log(track)
+        track.stop(); //disassociates the track with the source
+    })
+}
+
 document.querySelector('#share').addEventListener('click',e=>getMicAndCamera(e))
+document.querySelector('#show-video').addEventListener('click',e=>showMyFeed(e))
+document.querySelector('#stop-video').addEventListener('click',e=>stopMyFeed(e))
