2c18862821eabe8ae3b085a8cab518551add3807
Robert Bunch <rdbunch@gmail.com>
2023-06-15 18:28:28 -0400

    Beginning of 25. - add stun server config - createPeerConnection - createOffer() - add icecandidate listener

--- signalingPeerConnection/scripts.js
+++ signalingPeerConnection/scripts.js
@@ -7,6 +7,17 @@ let localStream; //a var to hold the local video stream
 let remoteStream; //a var to hold the remote video stream
 let peerConnection; //the peerConnection that the two clients use to talk
 
+let peerConfiguration = {
+    iceServers:[
+        {
+            urls:[
+              'stun:stun.l.google.com:19302',
+              'stun:stun1.l.google.com:19302'
+            ]
+        }
+    ]
+}
+
 //when a client initiates a call
 const call = async e=>{
     const stream = await navigator.mediaDevices.getUserMedia({
@@ -14,7 +25,36 @@ const call = async e=>{
         // audio: true,
     });
     localVideoEl.srcObject = stream;
+    localStream = stream;
+
+    //peerConnection is all set with our STUN servers sent over
+    await createPeerConnection();
+
+    //create offer time!
+    try{
+        console.log("Creating offer...")
+        const offer = await peerConnection.createOffer();
+        console.log(offer);
+    }catch(err){
+        console.log(err)
+    }
+
 }
+
+const createPeerConnection = ()=>{
+    return new Promise(async(resolve, reject)=>{
+        //RTCPeerConnection is the thing that creates the connection
+        //we can pass a config object, and that config object can contain stun servers
+        //which will fetch us ICE candidates
+        peerConnection = await new RTCPeerConnection(peerConfiguration)
+        peerConnection.addEventListener('icecandidate',e=>{
+            console.log('........Ice candidate found!......')
+            console.log(e)
+        })
+        resolve();
+    })
+}
+
 
 
 
