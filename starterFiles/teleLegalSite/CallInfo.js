67e36fb5da8d6f1d0a3ee74da4ab240222330721
Robert Bunch <rdbunch@gmail.com>
2023-07-05 16:16:47 -0400

    Update starter fikes for teleSite

---
+++ starterFiles/teleLegalSite/CallInfo.js
@@ -0,0 +1,29 @@
+import moment from 'moment'
+import { useEffect, useState } from 'react'
+
+const CallInfo = ({apptInfo})=>{
+
+    const [ momentText, setMomentText ] = useState(moment(apptInfo.apptDate).fromNow())
+    
+    useEffect(() => {
+        const timeInterval = setInterval(()=>{
+            setMomentText(moment(apptInfo.apptDate).fromNow())
+            console.log("Updating time")
+        },5000)
+        return () => {
+            console.log("Clearing")
+          clearInterval(timeInterval);
+        };
+      }, []);
+
+    return(
+        <div className="call-info">
+            <h1>
+                {apptInfo.professionalsFullName} has been notified.<br />
+                Your appointment is {momentText}.
+            </h1>
+        </div>
+    )
+}
+
+export default CallInfo
---
+++ starterFiles/teleLegalSite/callStatusReducer.js
@@ -0,0 +1,22 @@
+const initState = {
+    current: "idle",
+    video: false,
+    audio: false,
+    audioDevice: 'default',
+    videoDevice: 'default',
+    shareScreen: false,
+    haveMedia: false,
+}
+
+export default (state = initState, action)=>{
+    if (action.type === "UPDATE_CALL_STATUS"){
+        const copyState = {...state}
+        copyState[action.payload.prop] = action.payload.value
+        return copyState
+    }else if((action.type === "LOGOUT_ACTION") || (action.type === "NEW_VERSION")){
+        return "idle"
+    }else{
+        return state
+    }
+}
+
