aeff90a2755e18fa5393809321efc8994780ffc0
Robert Bunch <rdbunch@gmail.com>
2023-07-12 17:55:13 -0400

    Beginning of 63 update - missed File

--- teleLegalSite/teleLegal-back-end/socketServer.js
+++ teleLegalSite/teleLegal-back-end/socketServer.js
@@ -1,7 +1,24 @@
 //all our socketServer stuff happens here
 const io = require('./server').io;
+const app = require('./server').app;
+
+// const professionalAppointments = app.get('professionalAppointments')
+
+const allKnownOffers = {
+    // uniqueId - key
+    //offer
+    //professionalsFullName
+    //clientName
+    //apptDate
+    //offererIceCandidates
+    //answer
+    //answerIceCandidates
+};
 
 io.on('connection',socket=>{
     console.log(socket.id,"has connected")
+    socket.on('newOffer',({offer, apptInfo})=>{
+        //offer = sdp/type, apptInfo has the uuid that we can add to allKnownOffers
+        //so that, the professional can find EXACTLY the right allKnownOffers
+    })
 })
-
