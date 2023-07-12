93c48ae9add868045655034cc7f5e985756bd22f
Robert Bunch <rdbunch@gmail.com>
2023-07-11 23:55:01 -0400

    Beginning of 60 - add logic to handle audio and video dropdown - update AudioButton to manage itself - add type as a prop to DropDown

--- teleLegalSite/telelegal-front-end/src/videoComponents/ActionButtonCaretDropDown.js
+++ teleLegalSite/telelegal-front-end/src/videoComponents/ActionButtonCaretDropDown.js
@@ -1,12 +1,31 @@
 
-const ActionButtonCaretDropDown = ({defaultValue,changeHandler,deviceList})=>{
-        return(
-            <div className="caret-dropdown" style={{top:"-25px"}}>
-                <select defaultValue={defaultValue} onChange={changeHandler}>
-                    {deviceList.map(vd=><option key={vd.deviceId} value={vd.deviceId}>{vd.label}</option>)}
-                </select>
-            </div>
-        )
+const ActionButtonCaretDropDown = ({defaultValue,changeHandler,deviceList,type})=>{
+        
+    let dropDownEl;
+    if(type==="video"){
+        dropDownEl = deviceList.map(vd=><option key={vd.deviceId} value={vd.deviceId}>{vd.label}</option>)
+    }else if(type === "audio"){
+        const audioInputEl = [];
+        const audioOutputEl = [];
+        deviceList.forEach((d,i)=>{
+            if(d.kind === "audioinput"){
+                audioInputEl.push(<option key={d.deviceId} value={d.deviceId}>{d.label}</option>)
+            }else if(d.kind === "audiooutput"){
+                audioOutputEl.push(<option key={d.deviceId} value={d.deviceId}>{d.label}</option>)
+            }
+        })
+        audioInputEl.unshift(<optgroup label="Input Devices" />)
+        audioOutputEl.unshift(<optgroup label="Output Devices" />)
+        dropDownEl = audioInputEl.concat(audioOutputEl)
+    }
+    
+    return(
+        <div className="caret-dropdown" style={{top:"-25px"}}>
+            <select defaultValue={defaultValue} onChange={changeHandler}>
+                {dropDownEl}
+            </select>
+        </div>
+    )
 }
 
 export default ActionButtonCaretDropDown
--- teleLegalSite/telelegal-front-end/src/videoComponents/AudioButton/AudioButton.js
+++ teleLegalSite/telelegal-front-end/src/videoComponents/AudioButton/AudioButton.js
@@ -1,8 +1,13 @@
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux"
+import ActionButtonCaretDropDown from "../ActionButtonCaretDropDown";
+import getDevices from "../VideoButton/getDevices";
 
 const AudioButton = ()=>{
 
     const callStatus = useSelector(state=>state.callStatus);
+    const [ caretOpen, setCaretOpen ] = useState(false);
+    const [ audioDeviceList, setAudioDeviceList ] = useState([]);
 
     let micText;
     if(callStatus.current === "idle"){
@@ -13,13 +18,35 @@ const AudioButton = ()=>{
         micText = "Unmute"
     }
 
+    useEffect(()=>{
+        const getDevicesAsync = async()=>{
+            if(caretOpen){
+                //then we need to check for audio devices
+                const devices = await getDevices();
+                console.log(devices.videoDevices)
+                setAudioDeviceList(devices.audioOutputDevices.concat(devices.audioInputDevices))
+            }
+        }
+        getDevicesAsync()
+    },[caretOpen])
+
+    const changeAudioDevice = ()=>{
+
+    }
+
     return(
         <div className="button-wrapper d-inline-block">
-            <i className="fa fa-caret-up choose-audio"></i>
+            <i className="fa fa-caret-up choose-audio" onClick={()=>setCaretOpen(!caretOpen)}></i>
             <div className="button mic">
                 <i className="fa fa-microphone"></i>
                 <div className="btn-text">{micText}</div>
             </div>
+            {caretOpen ? <ActionButtonCaretDropDown 
+                            defaultValue={callStatus.audioDevice} 
+                            changeHandler={changeAudioDevice}
+                            deviceList={audioDeviceList}
+                            type="audio"
+                        /> : <></>}
         </div>        
     )
 }
