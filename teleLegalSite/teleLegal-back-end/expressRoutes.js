8c075b4674db2b30059b23e2b51bc0acdac76c43
Robert Bunch <rdbunch@gmail.com>
2023-07-12 17:54:58 -0400

    Beginning of 63 - add professionalAppionts array - add uuid - add allKnownOffersArray - add newOffer listener

--- teleLegalSite/teleLegal-back-end/expressRoutes.js
+++ teleLegalSite/teleLegal-back-end/expressRoutes.js
@@ -2,18 +2,27 @@
 const app = require('./server').app;
 const jwt = require('jsonwebtoken');
 const linkSecret = "ijr2iq34rfeiadsfkjq3ew";
+const { v4: uuidv4 } = require('uuid');
 
+const professionalAppointments = [];
+
+app.set('professionalAppointments',professionalAppointments)
 
 //this route is for US! In production, a receptionist, or calender/scheduling app
 //would send this out. We will print it out and paste it in. It will drop
 //us on our React site with the right info for CLIENT1 to make an offer
 app.get('/user-link',(req, res)=>{
 
+    const uuid = uuidv4(); //this is standing in as what would be a unique id/primary key in the db
+
     //data for the end-user's appt
     const apptData = {
         professionalsFullName: "Robert Bunch, J.D.",
-        apptDate: Date.now() + 500000
+        apptDate: Date.now() + 500000,
+        uuid,
     }
+
+    professionalAppointments.push(apptData);
 
     //we need to encode this data in a token
     //so it can be added to a url
@@ -29,4 +38,7 @@ app.post('/validate-link',(req, res)=>{
     const decodedData = jwt.verify(token,linkSecret);
     //send the decoded data (our object) back to the front end
     res.json(decodedData)
+
+    console.log(professionalAppointments)
+
 })
