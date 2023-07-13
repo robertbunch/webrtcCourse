54f3c5e1d5ffb93f20b5febb595bc9c68575eb65
Robert Bunch <rdbunch@gmail.com>
2023-07-13 11:06:25 -0400

    Update starter files for dashboard

--- starterFiles/teleLegalSite/ProDashboard.js
+++ starterFiles/teleLegalSite/ProDashboard.js
@@ -29,20 +29,22 @@ const ProDashboard = ()=>{
                 <div className="col-8">
                     <div className="row">
                         <h1>Dashboard</h1>
-                        <div className="col-6">
-                            <div className="dash-box clients-board orange-bg">
-                                <h4>Clients</h4>
-                                <li className="client">Jim Jones</li>
+                        <div className="row num-1">
+                            <div className="col-6">
+                                <div className="dash-box clients-board orange-bg">
+                                    <h4>Clients</h4>
+                                    <li className="client">Jim Jones</li>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-6">
-                            <div className="dash-box clients-board blue-bg">
-                                <h4>Coming Appointments</h4>
-                                <li className="client">Akash Patel - 8-10-23 11am <div className="waiting-text d-inline-block">Waiting</div><button className="btn btn-danger join-btn">Join</button></li>
-                                <li className="client">Jim Jones - 8-10-23, 2pm</li>
-                                <li className="client">Mike Williams - 8-10-23 3pm</li>
+                            <div className="col-6">
+                                <div className="dash-box clients-board blue-bg">
+                                    <h4>Coming Appointments</h4>
+                                    <li className="client">Akash Patel - 8-10-23 11am <div className="waiting-text d-inline-block">Waiting</div><button className="btn btn-danger join-btn">Join</button></li>
+                                    <li className="client">Jim Jones - 8-10-23, 2pm</li>
+                                    <li className="client">Mike Williams - 8-10-23 3pm</li>
+                                </div>
+                                
                             </div>
-                            
                         </div>
                         <div className="row num-2">
                             <div className="col-6">
@@ -52,6 +54,17 @@ const ProDashboard = ()=>{
                                     <div className="pointer"><i className="fa fa-plus"></i> file</div>
                                 </div>
                             </div>
+                            <div className="col-6">
+                                <div className="dash-box clients-board redish-bg">
+                                    <h4>Analytics</h4>
+                                    <div className="text-center">
+                                        <img src="https://s3.amazonaws.com/robertbunch.dev.publicresources/722443_infographic07.jpg" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            
+
                         </div>
                     </div>
                     <div className="row num-2">
