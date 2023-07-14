1e678c90d38a4c7e05b7b340c1f67297bd7aa20d
Robert Bunch <rdbunch@gmail.com>
2023-07-14 14:37:28 -0400

    Add seed data starter file

---
+++ starterFiles/teleLegalSite/apptSeedData.js
@@ -0,0 +1,15 @@
+const professionalAppointments = [{
+    professionalsFullName: "Peter Chan, J.D.",
+    apptDate: Date.now() + 500000,
+    uuid:1,
+    clientName: "Jim Jones",
+},{
+    professionalsFullName: "Peter Chan, J.D.",
+    apptDate: Date.now() - 2000000,
+    uuid:2,// uuid:uuidv4(),
+    clientName: "Akash Patel",
+},{
+    professionalsFullName:  "Peter Chan, J.D.",
+    apptDate: Date.now() + 10000000,
+    uuid:3,//uuid:uuidv4(),
+    clientName:
